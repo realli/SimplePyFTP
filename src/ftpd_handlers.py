407c274c0f4fb1d6c2d9a226ca01d912f5808b6e
leo <lrlanswer@gmail.com>
2013-04-13 22:26:24 +0800

    added the logging for file download (or fail),upload (or fail)

--- src/ftpd_handlers.py
+++ src/ftpd_handlers.py
@@ -3,11 +3,28 @@ import ftpd_log
 import logging
 
 class SimpleHandler(FTPHandler):
-  logger = logging.getLogger('visit')
+  '''
+  Simple Handler to hand the messages about login, logout , logout , file_received ....
+  all user ops which can be handed
+  '''
+  visit_logger = logging.getLogger(ftpd_log.logsDict['visitLog']['name'])
+  file_ops_logger = logging.getLogger(ftpd_log.logsDict['fileOpsLog']['name'])
   def on_login(self, username):
-    self.logger.info("%s - log in - %s", username,self.remote_ip)
+    SimpleHandler.visit_logger.info("%s - log in - %s", username,self.remote_ip)
   def on_logout(self, username):
-    self.logger.info("%s - log out - %s", username,self.remote_ip)
+    SimpleHandler.visit_logger.info("%s - log out - %s", username,self.remote_ip)
   def on_login_failed(self, username, password):
-    self.logger.info("%s - log in failed - %s", username,self.remote_ip)
+    SimpleHandler.visit_logger.info("%s - log failed - %s", username,self.remote_ip)
+  def on_file_sent(self, file):
+    SimpleHandler.file_ops_logger.info("%s - downloaded %s - %s", self.username,file,self.remote_ip)
+  def on_file_received(self, file):
+    SimpleHandler.file_ops_logger.info("%s - uploaded %s - %s", self.username,file,self.remote_ip)
+  def in_incomplete_file_sent(self, file):
+    SimpleHandler.file_ops_logger.info("%s - downloaded failed: %s - %s", self.username,file,self.remote_ip)
+  def on_incomplete_file_received(self, file):
+    import os
+    os.remove(file)
+    SimpleHandler.file_ops_logger.info("%s - uploaded failed: %s,already been deleted - %s", self.username,file,self.remote_ip)
+
+
 
--- src/ftpd_log.py
+++ src/ftpd_log.py
@@ -1,22 +1,38 @@
 import logging
 from logging.handlers import RotatingFileHandler
 
-def set_visit_log():
-  # set the visit log system
-  logger = logging.getLogger('visit')
+# logsDict stores the loggers' info , simplly as name , file to log and the format strings
+logsDict = {
+    'visitLog': {'name':'visit', 'fileName':'simple_ftpd_visit.log','fmt':" %(levelname)s:%(asctime)s - %(message)s"},
+    'fileOpsLog': {'name':'fileOps', 'fileName':'simple_ftpd_fileops.log','fmt':"%(levelname)s:%(asctime)s - %(message)s"}
+    }
+
+
+# used to set the logging system
+def set_log(name='visit',fileName='ftpd_log_unknow.log',fmt="%(levelname)s:%(asctime)s - %(message)s"):
+  '''
+  set the logger 'name' to log into file 'filename' and use the format 'fmt'
+  the logger all use Rotating File to dispatch messages which will send messages 
+  to disk file and rotating the file based the maximum log file size.
+  '''
+  logger = logging.getLogger(name)
   logger.setLevel(logging.DEBUG)
   logger.propagate=0
 
-  ch = RotatingFileHandler('simple_ftpd_visit.log',
+  ch = RotatingFileHandler(fileName,
                                   mode='a',
                                   maxBytes=10*1024*1024,
                                   backupCount=5
                                    )
   ch.setLevel(logging.DEBUG)
 
-  formatter = logging.Formatter('%(asctime)s - %(message)s')
+  formatter = logging.Formatter(fmt)
   ch.setFormatter(formatter)
 
   logger.addHandler(ch)
-if not logging.getLogger().handlers:
-  set_visit_log()
+
+
+# used to set the logger when imported
+for __, logDict in logsDict.items():
+  if not logging.getLogger(logDict['name']).handlers:
+    set_log(logDict['name'], logDict['fileName'])
